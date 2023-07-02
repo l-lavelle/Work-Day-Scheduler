e6a8ba2108df15caf4ea536d8afcd6c386ff00ac
Lauren <lauren.m.lavelle@wmich.edu>
2023-07-02 09:47:06 -0400

    text for each hour saved to local storage and persists when refreshed

--- script.js
+++ script.js
@@ -7,12 +7,22 @@ $(function () {
     
   $("div.time-block").on("click", "button", function(event){
     $(event.target).css("background-color", "pink");
-    var githubEl=$(this).siblings("#input").val();
-    
+    var userInput=$(this).siblings("#input").val();
+    var schduleHour=$(this).siblings(".hour").text();
+
+    var trial={
+      hours:schduleHour,
+      text:userInput
+    };
+    localStorage.setItem(schduleHour,JSON.stringify(trial));
+    // var ser=JSON.parse(localStorage.getItem("9AM"));
+    // console.log(ser)
+
+
     
     // var id=$(this).siblings(".time-block")
-    console.log(githubEl);
-    console.log(id);
+  
+    // console.log(id);
  
 
 
@@ -25,10 +35,32 @@ $(function () {
 
 
 
- 
+var nine=JSON.parse(localStorage.getItem("9AM"));
+$("#hour-9").children("#input").append(nine["text"]);
 
+var ten=JSON.parse(localStorage.getItem("10AM"));
+$("#hour-10").children("#input").append(ten["text"]);
 
+var eleven=JSON.parse(localStorage.getItem("11AM"));
+$("#hour-11").children("#input").append(eleven["text"]);
 
+var twelve=JSON.parse(localStorage.getItem("12PM"));
+$("#hour-12").children("#input").append(twelve["text"]);
+
+var one=JSON.parse(localStorage.getItem("1PM"));
+$("#hour-1").children("#input").append(one["text"]);
+
+var two=JSON.parse(localStorage.getItem("2PM"));
+$("#hour-2").children("#input").append(two["text"]);
+
+var three=JSON.parse(localStorage.getItem("3PM"));
+$("#hour-3").children("#input").append(three["text"]);
+
+var four=JSON.parse(localStorage.getItem("4PM"));
+$("#hour-4").children("#input").append(four["text"]);
+
+var five=JSON.parse(localStorage.getItem("5PM"));
+$("#hour-5").children("#input").append(five["text"]);
   });
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
